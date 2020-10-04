a2ff27a31352c5192fe735b46745d3f128fb57ee
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-10-04 16:46:13 +1100

    feat(page): updated component and search basics

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,12 @@
         <div class="columns is-multiline px-3 py-3">
           <NavCard class="column is-full" />
           <SearchCard class="column is-full is-hidden-desktop" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
-          <ListingCard class="column is-one-third-desktop is-half is-narrow" />
+          <ListingCard
+            v-for="listing in listings"
+            :key="listing.id"
+            :listing="listing"
+            class="column is-one-third-desktop is-half is-narrow"
+          />
         </div>
       </div>
     </div>
@@ -34,5 +34,49 @@ export default {
     SearchCard,
     NavCard,
   },
+  async fetch() {
+    const queries = this.$route.query
+
+    if (queries.search) {
+      queries.titleiLike = queries.search
+
+      delete queries.search
+    }
+
+    if (queries.category) {
+      if (Array.isArray(queries.category)) {
+        queries.category.forEach((query, index) => {
+          queries[`categoryRefId[${index}]`] = query
+        })
+      } else {
+        queries['categoryRefId[]'] = queries.category
+      }
+
+      delete queries.category
+    }
+
+    const queryString = Object.keys(queries)
+      .map((key) => key + '=' + queries[key])
+      .join('&')
+
+    await this.$axios.get(`/api/v1_0/listings?${queryString}`).then((res) => {
+      this.listings = res.data.data.listings
+    })
+  },
+  asyncData({ query, $axios }) {
+    return $axios.get(`/api/v1_0/listings`).then((res) => {
+      return { listings: res.data.data.listings }
+    })
+  },
+  watchQuery: [
+    'search',
+    'priceGte',
+    'priceLte',
+    'category',
+    'orderBy',
+    'filterBy',
+    'skip',
+    'take',
+  ],
 }
 </script>
