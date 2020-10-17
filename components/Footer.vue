5227c610ab36b0c4ab3c6c06e5c9c6e45a0386f2
Saber Ali <saber_ali04@outlook.com>
2020-10-17 20:54:07 +1100

    refactor(component): updated footer to include links to new pages

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="footer">
     <div class="row has-text-centered">
-      {{ new Date().getFullYear() }} — <strong>LushCloset</strong>
+      <a href="about" class="footer-links">About</a>
+      <a href="/mobile-app-invite" class="footer-links">Mobile App</a>
+      <a href="contact-us" class="footer-links">Contact Us</a>
+      <div class="company has-text-centered">
+        {{ new Date().getFullYear() }} — <strong>LushCloset</strong>
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +19,13 @@
   background-color: yellowgreen
   color: white
 
+.footer-links
+  color: white
+  padding: 10px
+
 .footer strong
   color: white
+
+.company
+  text-align: right
 </style>
